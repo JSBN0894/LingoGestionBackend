e72beee81027150715f9e26dd06e13dd38377b67
Jsbn <johan.sebastian.becerra@gmail.com>
2026-03-05 01:39:53 -0500

    Ajuste para creacion correcta de tablas

--- gestion/src/main/kotlin/com/linogo/gestion/GestionApplication.kt
+++ gestion/src/main/kotlin/com/linogo/gestion/GestionApplication.kt
@@ -1,9 +1,13 @@
 package com.linogo.gestion
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EntityScan("com.linogo.gestion")
+@EnableJpaRepositories("com.linogo.gestion")
 class GestionApplication
 
 fun main(args: Array<String>) {
